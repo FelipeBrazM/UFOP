97ea3eff5e1883adbfdf4894f3bb0e65883a37eb
FelipeBrazM <felipebrazmarques@gmail.com>
2023-10-01 19:35:22 -0300

    Codigo TP So pronto

--- Sistema Operacionais/TP1/tp1.c
+++ Sistema Operacionais/TP1/tp1.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 void QuebraLinha(){
-    printf("\n");
+    printf("\n\n");
 }
 
 int main(){
@@ -11,7 +11,11 @@ int main(){
     char comando[32];
     char matar[32] = "kill -9 ";
     char prioridade[32] = "renice 10 -p ";
+    char matarTodos[32] = "killall ";
+    char pMatar[32] = "pkill ";
 
+    QuebraLinha();
+    
     //Exibindo a versão do Ubuntu 
     system("cat /etc/os-release");
     QuebraLinha();
@@ -39,23 +43,35 @@ int main(){
     system(matar);
     QuebraLinha();
     system("ps");
+    QuebraLinha();
     
     //Executando o comando killall
-    system("killall");
+    printf("Entre com nome do programa que você deseja dar o killall: ");
+    scanf("%s", comando);
+    strcat(matarTodos,comando);
+    system(matarTodos);
     QuebraLinha();
-
-    //Executando o comando pkill
-    system("pkill");
+    system("ps");
     QuebraLinha();
 
     //Executando o comando renice
     system("top");
+    QuebraLinha();
     printf("Entre com o valor do PID que você deseja passar para a prioridade 10: ");
     scanf("%s", comando);
     strcat(prioridade,comando);
     system(prioridade);
     QuebraLinha();
     system("top");
+    QuebraLinha();
+
+    //Executando o comando pkill
+    printf("Entre com nome do programa que você deseja dar o pKill: ");
+    scanf("%s", comando);
+    strcat(pMatar,comando);
+    system(pMatar);
+    QuebraLinha();
+    system("ps");
 
     return 0;
 }
